9fbae4f0ba2e65cfe618da96c622ae15d633c42c
miguelarian <miguelvr88@gmail.com>
2024-03-08 14:13:07 +0100

    initialise spring project

---
+++ src/test/java/com/miguelvela/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.miguelvela.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This method is empty, but it's intended to check if the context loads successfully.
+	}
+
+}
