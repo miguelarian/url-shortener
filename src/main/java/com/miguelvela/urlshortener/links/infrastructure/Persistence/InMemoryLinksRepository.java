d9f13a233712c0e927c2838cea4bade6b016c70c
miguelarian <miguelvr88@gmail.com>
2024-03-10 17:19:57 +0100

    add dummy in-memory Links repository

---
+++ src/main/java/com/miguelvela/urlshortener/links/infrastructure/Persistence/InMemoryLinksRepository.java
@@ -0,0 +1,34 @@
+package com.miguelvela.urlshortener.links.infrastructure.Persistence;
+
+import com.miguelvela.urlshortener.links.domain.Link;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class InMemoryLinksRepository implements LinksRepository {
+
+    private List<Link> inMemoryLinks = new ArrayList<>(Arrays.asList(
+            new Link("https://www.example.com", "Example"),
+            new Link("https://www.google.com", "Google"),
+            new Link("https://www.github.com", "GitHub")
+    ));
+
+    @Override
+    public List<Link> GetLinks() {
+
+        return this.inMemoryLinks;
+    }
+
+    @Override
+    public Link GetByLinkId(String linkId) {
+
+        return this.inMemoryLinks
+                .stream()
+                .filter(link -> link.getLinkId().equals(linkId))
+                .findFirst()
+                .orElse(null);
+    }
+}
