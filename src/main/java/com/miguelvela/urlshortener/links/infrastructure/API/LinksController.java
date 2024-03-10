c93945a072d8bd37c5094f422870c252eeb9e70a
miguelarian <miguelvr88@gmail.com>
2024-03-10 21:02:28 +0100

    update imports

--- src/main/java/com/miguelvela/urlshortener/links/infrastructure/API/LinksController.java
+++ src/main/java/com/miguelvela/urlshortener/links/infrastructure/API/LinksController.java
@@ -4,7 +4,9 @@ import com.miguelvela.urlshortener.links.application.LinksService;
 import com.miguelvela.urlshortener.links.domain.Link;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
