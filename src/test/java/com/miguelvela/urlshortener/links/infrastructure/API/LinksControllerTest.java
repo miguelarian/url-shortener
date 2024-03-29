4f2d5bc3de76969fbb14f412e19094aeacbba272
miguelarian <miguelvr88@gmail.com>
2024-03-29 20:19:36 +0100

    remove @SpringBootTest when is not necessary

--- src/test/java/com/miguelvela/urlshortener/links/infrastructure/API/LinksControllerTest.java
+++ src/test/java/com/miguelvela/urlshortener/links/infrastructure/API/LinksControllerTest.java
@@ -19,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class LinksControllerTest {
+class LinksControllerTest {
 
     @InjectMocks
     LinksController linksController;
