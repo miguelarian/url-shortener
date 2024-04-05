288e6d5a2389feac543d62f5a87858397fab963e
miguelarian <miguelvr88@gmail.com>
2024-04-05 12:37:20 +0200

    add log4j2 and slf4j

--- src/main/java/com/miguelvela/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/miguelvela/urlshortener/UrlShortenerApplication.java
@@ -1,11 +1,16 @@
 package com.miguelvela.urlshortener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UrlShortenerApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(UrlShortenerApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerApplication.class, args);
+		LOGGER.info(UrlShortenerApplication.class.toString() + " started...");
 	}
 }
